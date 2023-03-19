05125f9ee5bcfa7354ad4e2f8cd885d95335a100
Acelven <acelven@hotmail.com>
2023-03-19 02:45:04 +0000

    excuse generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
+window.onload = () => {
+  let randomNumber = Math.random() * 10;
+  document.querySelector("#the-excuse").innerHTML = generateExcuse();
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  let subject = ["The dog", "A horse", "A driver", "A homeless", "A clown"];
+  let action = [
+    "took my",
+    "destroyed my",
+    "sat on my",
+    "blew up my",
+    "stole my"
+  ];
+  let what = ["homework", "car", "computer", "cellphone"];
+  let location = ["on the street", "in my room", "in my home", "in my garden"];
+
+  let subjectIndex = Math.floor(Math.random() * subject.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let locationIndex = Math.floor(Math.random() * location.length);
+
+  return (
+    subject[subjectIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    location[locationIndex]
+  );
 };
